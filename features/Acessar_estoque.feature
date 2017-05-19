35a1da6f2d5a2f290f45c3e78345f143edc4bb65
elther <edso@cin.ufpe.br>
2017-05-19 19:18:19 +0000

    mais BDD ainda incompleto

---
+++ features/Acessar_estoque.feature
@@ -0,0 +1,9 @@
+Feature: Controle de Fluxo
+  Como um usu�rio do sistema,
+  Para que  eu movimente meu estoque de maneira segura e precisa,
+  Eu quero um sistema que me forne�a a entrada e sa�da de materiais.
+  
+  Scenario: Control flow
+    Given I logged 
+    When I click em see all
+    Then i should see all inputs e outputs
---
+++ features/step_definitions/Acessar_estoque_step.rb
@@ -0,0 +1,11 @@
+Given(/^I logged$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click em see all$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^i should see all inputs e outputs$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
